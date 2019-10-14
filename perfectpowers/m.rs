ee069ed1031d9f432960f128afb13d2d0a740b7d
Mårten Nilsson <m@martennilsson.se>
2019-10-14 20:56:50 +0200

    Some beautifications

--- perfectpowers/m.rs
+++ perfectpowers/m.rs
@@ -5,7 +5,13 @@ fn max_pow(x: i32) -> String{
         _ if x < 0 => (2, -x),
         _ => (1, x),
     };
-    format!("{}\n", (1..33).step_by(step).filter(|pow| (pos as f64).powf(1. / (*pow as f64)).round().powi(*pow) as i32 == pos).last().unwrap())
+    format!("{}\n",
+            (1..33)
+            .step_by(step)
+            .filter(|pow| (pos as f64).powf(1. / (*pow as f64)).round().powi(*pow) as i32 == pos)
+            .last()
+            .unwrap()
+           )
 }
 
 fn main(){
